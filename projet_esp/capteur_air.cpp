1edb4cd2ddd2bac377a220d44bdc742f5d83f1e4
Branlix2OOO <juliendumas2002@gmail.com>
2023-12-05 08:13:17 +0100

    nkjfn

--- projet_esp/capteur_air.cpp
+++ projet_esp/capteur_air.cpp
@@ -1,4 +1,4 @@
-#include "AirQuality.h"
+
 #include "Arduino.h"
 #include "capteur_air.h"
 
@@ -11,7 +11,7 @@ void capteur_air::capteur_air(String pin){
 void capteur_air::init_capteur_air(){
  
   while(!(etatinit)){
-    this->etatinit = this->sensor.init();
+    this->etatinit = this->sensor.init(this->pin);
   }
   Serial.println("Sensor ready.");
 }
@@ -21,5 +21,14 @@ int get_air_values(){
     Serial.println("Sensor not initialized !!!");
     exit(-1);
   }
-  return this->sensor.slope();
+  return this->sensor.slope(); //renvoie une valeur de 0 à 3, 0 =très pollué
+}
+
+String get_string(){
+  int temp = get_air_values()
+  if (temp=0) return "Air très très pollué";
+  else if (temp=1) return "Air très pollué";
+  else if (temp=2) return "Air très pollué";
+  else if (temp=3) return "Air frais";
+  else return "erreur valeur"
 }
--- projet_esp/capteur_air.h
+++ projet_esp/capteur_air.h
@@ -1,16 +1,17 @@
-#include "Air_Quality_Sensor.h"
+#include <Arduino.h>
 
 #ifndef _CAPTEUR_AIR_
 #define _CAPTEUR_AIR_
 
 class capteur_air {
 
-  char pin;
+  String pin;
   bool etatinit;
   AirQualitySensor sensor;
   public :
   void init_capteur_air();
   int get_air_values();
+  String get_string();
 
 }
 
